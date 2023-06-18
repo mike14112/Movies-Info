609b31268b44f58fa5218a5db452ae56d17f5b28
Mike <89590104+mike14112@users.noreply.github.com>
2023-06-18 13:48:44 +0400

    fixed path name

--- js/2022.js
+++ js/2022.js
@@ -31,7 +31,7 @@ const getMovie = async  function (num) {
     <p class="card-text  text-center genre"> Жанр :${films[i].genres[0].name}</p>
     <p class="card-text  text-center about "> Описание : ${films[i].description.substring(0,200)}... </p>
     </div>
-    <a href="/docs/filmsId.html?films=${films[i].id}"   class="btn btn-primary  mb-2">Перейти куда-нибудь</a>
+    <a href="filmsId.html?films=${films[i].id}"   class="btn btn-primary  mb-2">Перейти куда-нибудь</a>
     
     </div> 
 
